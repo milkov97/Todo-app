5589eb61dc0cab239863c8833b08f74bd84325a8
milkov97 <129657588+milkov97@users.noreply.github.com>
2023-08-04 15:17:42 +0300

    initialize database

--- api/index.ts
+++ api/index.ts
@@ -1,15 +1,32 @@
-import express, { Express, Request, Response } from "express";
-import dotenv from "dotenv"
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+import { DataSource } from 'typeorm';
+
 
 const app: Express = express();
 dotenv.config();
 
-const port = process.env.PORT;
-
-
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + Typescript Server");
+export const AppDataSource = new DataSource({
+  type: 'mysql',
+  host: 'localhost',
+  port: 3306,
+  username: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DB,
+  synchronize: true,
 });
 
-app.listen(port);
+const port = process.env.PORT;
 
+app.get('/', (req: Request, res: Response) => {
+  res.send('Express + Typescript Server');
+});
+
+AppDataSource.initialize()
+  .then(() => {
+    app.listen(port);
+    console.log('Data Source has been initialized!');
+  })
+  .catch((err) => {
+    console.error('Error during Data Source initialization', err);
+  });
