85da157d5286d01cc81289a35943190ff7f54d79
milkov97 <129657588+milkov97@users.noreply.github.com>
2023-08-04 16:11:43 +0300

    add columns using typeorm properties

--- api/src/tasks/tasks.entity.ts
+++ api/src/tasks/tasks.entity.ts
@@ -1,9 +1,34 @@
-import { Entity, PrimaryGeneratedColumn } from "typeorm"
-// import { Priority } from "../enums/Priority"
-// import { Status } from "../enums/Status"
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { Priority } from '../enums/Priority';
+import { Status } from '../enums/Status';
 
 @Entity()
 export class Task {
-    @PrimaryGeneratedColumn('uuid')
-    id: string;
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+  @Column({
+    type: 'text',
+  })
+  title: string;
+  @Column({
+    type: 'varchar',
+    length: 255,
+  })
+  date: string;
+  @Column({
+    type: 'longtext',
+  })
+  description: string;
+  @Column({
+    type: 'enum',
+    enum: Priority,
+    default: Priority.normal,
+  })
+  priority: Priority;
+  @Column({
+    type: 'enum',
+    enum: Status,
+    default: Status.todo,
+  })
+  status: Status;
 }
