21f9e676d1aae0c323bf4d942a6896b226276a3e
milkov97 <129657588+milkov97@users.noreply.github.com>
2023-08-04 21:33:19 +0300

    add post endoint and validator

--- api/src/tasks/task.controller.ts
+++ api/src/tasks/task.controller.ts
@@ -1,24 +1,28 @@
 import { AppDataSource } from '../../index';
 import { Task } from './tasks.entity';
+import { instanceToPlain } from 'class-transformer';
 
 export class TaskController {
   constructor(private taskRepository = AppDataSource.getRepository(Task)) {}
 
-  public async getAll(): Promise<Task[]> {
+
+  public async getAll(): Promise<Task[] | undefined> {
     // Declare a variable to hold all tasks
     let allTasks: Task[];
     // Fetch all tasks using the repository
     try {
       allTasks = await this.taskRepository.find({
         order: {
-            date: 'ASC',
+          date: 'ASC',
         },
-    });    
-    } catch(errors){
-        console.log(errors);
-        
+      });
+      // Convert the task instance to an array of objects
+      allTasks = instanceToPlain(allTasks) as Task[];
+      return allTasks;
+    } catch (errors) {
+      console.log(errors);
+      
     }
-    
-    // Convert the task instance to an array of objects
+    return undefined;
   }
 }
