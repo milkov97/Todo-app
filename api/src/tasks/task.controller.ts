cd4958049d4c7437ded1d1ee48e462171b24b6d2
milkov97 <129657588+milkov97@users.noreply.github.com>
2023-08-05 10:16:27 +0300

    refactoring router and controller modules

--- api/src/tasks/task.controller.ts
+++ api/src/tasks/task.controller.ts
@@ -1,28 +1,55 @@
+import { validationResult } from 'express-validator';
 import { AppDataSource } from '../../index';
 import { Task } from './tasks.entity';
 import { instanceToPlain } from 'class-transformer';
+import { Response, Request } from 'express';
 
-export class TaskController {
-  constructor(private taskRepository = AppDataSource.getRepository(Task)) {}
-
-
-  public async getAll(): Promise<Task[] | undefined> {
+class TaskController {
+  public async getAll(req: Request, res: Response): Promise<Response> {
     // Declare a variable to hold all tasks
     let allTasks: Task[];
     // Fetch all tasks using the repository
     try {
-      allTasks = await this.taskRepository.find({
+      allTasks = await AppDataSource.getRepository(Task).find({
         order: {
           date: 'ASC',
         },
       });
       // Convert the task instance to an array of objects
       allTasks = instanceToPlain(allTasks) as Task[];
-      return allTasks;
-    } catch (errors) {
-      console.log(errors);
-      
+      return res.json(allTasks).status(200);
+    } catch (_errors) {
+      return res.json({ error: 'Internal Server Error' }).status(500);
     }
-    return undefined;
+  }
+
+  public async create(req: Request, res: Response): Promise<Response> {
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const newTask = new Task();
+
+    newTask.title = req.body.title;
+    newTask.date = req.body.date;
+    newTask.description = req.body.description;
+    newTask.priority = req.body.priority;
+    newTask.status = req.body.status;
+
+    let createdTask: Task;
+    try {
+       createdTask = await AppDataSource.getRepository(Task)
+       .save(newTask);
+
+       createdTask = instanceToPlain(createdTask) as Task;
+
+       return res.json(createdTask).status(201);
+    }catch(errors) {
+      return res.json({ error: 'Internal Server Error' }).status(500);
+    }
   }
 }
+
+export const taskController = new TaskController();
--- api/src/tasks/tasks.router.ts
+++ api/src/tasks/tasks.router.ts
@@ -1,19 +1,10 @@
-import { Router, Request, Response } from 'express';
-import { TaskController } from './task.controller';
+import { Router } from 'express';
+import { taskController } from './task.controller';
 import { createValidator } from './tasks.validator';
-import { validationResult } from 'express-validator';
+
 
 export const tasksRouter: Router = Router();
 
-tasksRouter.get('/tasks', async (req: Request, res: Response) => {
-  const taskController = new TaskController();
-  const allTasks = await taskController.getAll();
-  res.json(allTasks).status(200);
-});
+tasksRouter.get('/tasks', taskController.getAll);
 
-tasksRouter.post('/tasks', createValidator, async (req: Request, res: Response) => {
-  const errors = validationResult(req);
-  if(!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
-  }
-});
+tasksRouter.post('/tasks', createValidator, taskController.create);
