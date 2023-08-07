bc64153a6dee38828b451774f7a053bfb085c832
milkov97 <129657588+milkov97@users.noreply.github.com>
2023-08-07 12:34:06 +0300

    ...

--- frontend/src/components/task/interfaces/TaskInterface.ts
+++ frontend/src/components/task/interfaces/TaskInterface.ts
@@ -1,8 +1,6 @@
 import { TaskFooterInterface } from "./TaskFooterInterface";
 import { TaskHeaderInterface } from "./TaskHeaderInterface";
-import { TaskDescription } from "../_taskDescription";
 import { TaskDescriptionInterface } from "./TaskDescriptionInterface";
-import { StringLiteral } from "typescript";
 
 export interface TaskInterface
   extends TaskHeaderInterface,
